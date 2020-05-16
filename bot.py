029a95c0587622cfb9b7018c7d57560a82c833a3
a855452 <51374780+a855452@users.noreply.github.com>
2020-05-17 01:47:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 import discord
 from discord.ext import commands
-
+import json
+import random
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 bot = commands.Bot(command_prefix=">")
 
 @bot.event
 async def on_ready():
     print(">>bot is online")
-bot.run("NzExMTEwODMxNzQzNjk2OTk2.Xr_tBQ.rbKuMZxNr7dGLSeboDvpdVcsKYQ")
+@bot.command()
+async def 抽(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file=pic)
+
+bot.run(jdata['TOKEN'])
